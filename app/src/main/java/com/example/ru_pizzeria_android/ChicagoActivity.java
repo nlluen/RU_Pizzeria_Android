dcf9a393dd775095c6e9151891f9f91a172552f0
Nick Lluen <98544701+nlluen@users.noreply.github.com>
2022-11-28 17:33:42 -0500

    displaying price

--- app/src/main/java/com/example/ru_pizzeria_android/ChicagoActivity.java
+++ app/src/main/java/com/example/ru_pizzeria_android/ChicagoActivity.java
@@ -14,21 +14,25 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.ru_pizzeria_android.src.ChicagoPizza;
 import com.example.ru_pizzeria_android.src.Pizza;
 import com.example.ru_pizzeria_android.src.PizzaFactory;
 
+import java.text.DecimalFormat;
+import java.util.Locale;
+
 public class ChicagoActivity extends AppCompatActivity {
     private Button mm_btn, topping_btn;
     private Spinner flavSpinner, sizeSpinner;
     private String [] flavors = {"Deluxe", "BBQ Chicken", "Meatzza", "Build Your Own"};
-    private String [] sizes = {"Small","Medium","Large"};
+    private String [] sizes = {"small","medium","large"};
     private ArrayAdapter<String> adapter;
     private ArrayAdapter<String> sizeAdapter;
-    private TextView crust_type;
+    private TextView crust_type, pizza_price;
     private Pizza pizza;
-    private String flavor;
+    private String flavor, size;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,22 +40,58 @@ public class ChicagoActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chicago);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+
         flavSpinner = findViewById(R.id.pizza_flavor);
         adapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, flavors);
         flavSpinner.setAdapter(adapter);
+        flavor = "Deluxe";
+
+        flavSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                flavor = flavSpinner.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(), flavor, Toast.LENGTH_LONG).show();
+                if (flavor.equalsIgnoreCase("Deluxe")) {
+                    selectDeluxe();
+                } else if (flavor.equalsIgnoreCase("BBQ Chicken")) {
+                    selectBBQChicken();
+                } else if (flavor.equalsIgnoreCase("Meatzza")) {
+                    selectMeatzza();
+                }  else if (flavor.equalsIgnoreCase("Build Your Own")) {
+                    selectBuildYourOwn();
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
         sizeSpinner = findViewById(R.id.size);
         sizeAdapter = new ArrayAdapter<String>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item,sizes);
         sizeSpinner.setAdapter(sizeAdapter);
-        flavSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+        //size = "large";
+
+        sizeSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                AlertDialog.Builder alert = new AlertDialog.Builder(ChicagoActivity.this);
-                alert.setTitle("Alert !");
-                AlertDialog dialog = alert.create();
-                dialog.show();
-                flavor = flavSpinner.getSelectedItem().toString();
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                size = sizeSpinner.getItemAtPosition(i).toString();
+                Toast.makeText(getApplicationContext(), size, Toast.LENGTH_LONG).show();
+                pizza.setSize(size.toLowerCase(Locale.ROOT));
+                DecimalFormat decimalFormat = new DecimalFormat("###,##0.00");
+                Toast.makeText(getApplicationContext(),decimalFormat.format(pizza.price()) , Toast.LENGTH_LONG).show();
+                pizza_price = (TextView) findViewById(R.id.pizza_price);
+                pizza_price.setText(String.valueOf(decimalFormat.format(pizza.price())));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
             }
         });
+
 
         topping_btn = (Button) findViewById(R.id.toppings);
         topping_btn.setOnClickListener(new View.OnClickListener() {
@@ -60,20 +100,6 @@ public class ChicagoActivity extends AppCompatActivity {
                 openToppingsActivity();
             }
         });
-
-        if (flavor.equalsIgnoreCase("Deluxe")) {
-            selectDeluxe();
-        } else if (flavor.equalsIgnoreCase("BBQ Chicken")) {
-            System.out.println("bbq");
-            selectBBQChicken();
-        } else if (flavor.equalsIgnoreCase("Meatzza")) {
-            System.out.println("meatzzas");
-            selectMeatzza();
-        }  else if (flavor.equalsIgnoreCase("Build Your Own")) {
-            selectBuildYourOwn();
-        }
-
-
     }
 
     @Override
@@ -96,6 +122,9 @@ public class ChicagoActivity extends AppCompatActivity {
         PizzaFactory pizzaFactory = new ChicagoPizza();
         pizza = pizzaFactory.createDeluxe();
         pizza.setCrust("(Chicago Style - Deep Dish");
+        DecimalFormat decimalFormat = new DecimalFormat("###,##0.00");
+        pizza_price = (TextView) findViewById(R.id.pizza_price);
+        pizza_price.setText(String.valueOf(decimalFormat.format(pizza.price())));
         crust_type = (TextView) findViewById(R.id.crust_type);
         crust_type.setText("Deep Dish");
     }
@@ -104,6 +133,9 @@ public class ChicagoActivity extends AppCompatActivity {
         PizzaFactory pizzaFactory = new ChicagoPizza();
         pizza = pizzaFactory.createBBQChicken();
         pizza.setCrust("(Chicago Style - Pan");
+        DecimalFormat decimalFormat = new DecimalFormat("###,##0.00");
+        pizza_price = (TextView) findViewById(R.id.pizza_price);
+        pizza_price.setText(String.valueOf(decimalFormat.format(pizza.price())));
         crust_type = (TextView) findViewById(R.id.crust_type);
         crust_type.setText("Pan");
     }
@@ -112,6 +144,9 @@ public class ChicagoActivity extends AppCompatActivity {
         PizzaFactory pizzaFactory = new ChicagoPizza();
         pizza = pizzaFactory.createMeatzza();
         pizza.setCrust("(Chicago Style - Stuffed");
+        DecimalFormat decimalFormat = new DecimalFormat("###,##0.00");
+        pizza_price = (TextView) findViewById(R.id.pizza_price);
+        pizza_price.setText(String.valueOf(decimalFormat.format(pizza.price())));
         crust_type = (TextView) findViewById(R.id.crust_type);
         crust_type.setText("Stuffed");
     }
@@ -120,6 +155,9 @@ public class ChicagoActivity extends AppCompatActivity {
         PizzaFactory pizzaFactory = new ChicagoPizza();
         pizza = pizzaFactory.createBuildYourOwn();
         pizza.setCrust("(Chicago Style - Pan");
+        DecimalFormat decimalFormat = new DecimalFormat("###,##0.00");
+        pizza_price = (TextView) findViewById(R.id.pizza_price);
+        pizza_price.setText(String.valueOf(decimalFormat.format(pizza.price())));
         crust_type = (TextView) findViewById(R.id.crust_type);
         crust_type.setText("Pan");
     }
